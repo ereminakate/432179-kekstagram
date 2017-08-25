2070d10d90d7152544ca6f5436bfc888eb1c9ffa
Ekaterina <popova_katyusha90@mail.ru>
2017-08-25 19:24:22 +0300

    Попытка выполнить 11 задание (реализация открытия и закрытия картинки из галереи) в файле picture.js

--- js/pictures.js
+++ js/pictures.js
@@ -4,18 +4,24 @@ var pictureTemplate = document.querySelector('#picture-template').content;
 
 var listPictures = document.querySelector('.pictures');
 
+var galleryOverlay = document.querySelector('.gallery-overlay');
 
 var USER_COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+var PHOTO_COUNT = 25;
+var LIKES_MIN = 15;
+var LIKES_MAX = 200;
+var COMMENTS_MIN = 1;
+var COMMENTS_MAX = 2;
 
 var pictures = createPictures();
 
 // Функция создания массива, состоящего из 25 сгенерированных JS объектов, которые будут описывать фотографии, размещенные другими пользователями
 function createPictures() {
   var arrPictures = [];
-  for (var i = 0; i < 25; i++) {
+  for (var i = 0; i < PHOTO_COUNT; i++) {
     arrPictures[i] = {
       url: './photos/' + (i + 1) + '.jpg',
-      likes: randomInteger(15, 200),
+      likes: randomInteger(LIKES_MIN, LIKES_MAX),
       comments: randomComments()
     };
   }
@@ -24,14 +30,12 @@ function createPictures() {
 
 // Функция выбора случайных чисел
 function randomInteger(min, max) {
-  var rand = min + Math.random() * (max + 1 - min);
-  rand = Math.floor(rand);
-  return rand;
+  return Math.floor(min + Math.random() * (max - min + 1));
 }
 
 // Функция выбора случайного комментария
 function randomComments() {
-  var numberComments = randomInteger(1, 2);
+  var numberComments = randomInteger(COMMENTS_MIN, COMMENTS_MAX);
   var comment = '';
   if (numberComments === 1) {
     comment += USER_COMMENTS[randomInteger(0, USER_COMMENTS.length - 1)];
@@ -60,9 +64,11 @@ listPictures.appendChild(fragment);
 
 document.querySelector('.upload-overlay').classList.add('hidden');
 
-var galleryOverlay = document.querySelector('.gallery-overlay');
-galleryOverlay.classList.remove('hidden');
+listPictures.addEventListener('click', function (evt) {
+  galleryOverlay.querySelector('.gallery-overlay-image').setAttribute('src', pictures[0].url);
+  galleryOverlay.querySelector('.likes-count').textContent = pictures[0].likes;
+  galleryOverlay.querySelector('.comments-count').textContent = pictures[0].comments;
+  galleryOverlay.classList.remove('hidden');
+  evt.target.stopPropagation();
+}, true);
 
-galleryOverlay.querySelector('.gallery-overlay-image').setAttribute('src', pictures[0].url);
-galleryOverlay.querySelector('.likes-count').textContent = pictures[0].likes;
-galleryOverlay.querySelector('.comments-count').textContent = pictures[0].comments;
