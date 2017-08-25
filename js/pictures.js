1fed5b8904246b76f88d9514555d5df0079ce474
Ekaterina <popova_katyusha90@mail.ru>
2017-08-25 23:02:17 +0300

    Ввыполнено 11 задание (реализовала открытие и закрытие картинки из галереи) в файле picture.js

--- js/pictures.js
+++ js/pictures.js
@@ -1,10 +1,9 @@
 'use strict';
 
 var pictureTemplate = document.querySelector('#picture-template').content;
-
 var listPictures = document.querySelector('.pictures');
-
 var galleryOverlay = document.querySelector('.gallery-overlay');
+var galleryOverlayClose = document.querySelector('.gallery-overlay-close');
 
 var USER_COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 var PHOTO_COUNT = 25;
@@ -12,11 +11,13 @@ var LIKES_MIN = 15;
 var LIKES_MAX = 200;
 var COMMENTS_MIN = 1;
 var COMMENTS_MAX = 2;
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
 
-var pictures = createPictures();
+var pictures = createDescriptionPictures();
 
 // Функция создания массива, состоящего из 25 сгенерированных JS объектов, которые будут описывать фотографии, размещенные другими пользователями
-function createPictures() {
+function createDescriptionPictures() {
   var arrPictures = [];
   for (var i = 0; i < PHOTO_COUNT; i++) {
     arrPictures[i] = {
@@ -34,7 +35,7 @@ function randomInteger(min, max) {
 }
 
 // Функция выбора случайного комментария
-function randomComments() {
+/* function randomComments() {
   var numberComments = randomInteger(COMMENTS_MIN, COMMENTS_MAX);
   var comment = '';
   if (numberComments === 1) {
@@ -43,6 +44,15 @@ function randomComments() {
     comment += USER_COMMENTS[randomInteger(0, USER_COMMENTS.length - 1)] + ' ' + USER_COMMENTS[randomInteger(0, USER_COMMENTS.length - 1)];
   }
   return comment;
+}*/
+
+function randomComments() {
+  var numberComments = randomInteger(COMMENTS_MIN, COMMENTS_MAX);
+  var comments = [];
+  for (var i = 0; i < numberComments; i++) {
+    comments.push(USER_COMMENTS[randomInteger(0, USER_COMMENTS.length - 1)]);
+  }
+  return comments + ' ' + numberComments;
 }
 
 // Функция заполнения данными фотографий
@@ -55,20 +65,77 @@ function renderPictures(pic) {
   return elementPicture;
 }
 
+// Вставка фотографии с данными в оверлей
+function setPhotoToOverlay(element) {
+  var elementImg = element.querySelector('img').getAttribute('src');
+  var elementLikes = element.querySelector('.picture-likes').textContent;
+  var elementComments = element.querySelector('.picture-comments').textContent;
+
+  galleryOverlay.querySelector('.gallery-overlay-image').setAttribute('src', elementImg);
+  galleryOverlay.querySelector('.likes-count').textContent = elementLikes;
+  galleryOverlay.querySelector('.comments-count').textContent = elementComments;
+}
+
 var fragment = document.createDocumentFragment();
+
 for (var i = 0; i < pictures.length; i++) {
   fragment.appendChild(renderPictures(pictures[i]));
 }
 
 listPictures.appendChild(fragment);
 
-document.querySelector('.upload-overlay').classList.add('hidden');
-
-listPictures.addEventListener('click', function (evt) {
-  galleryOverlay.querySelector('.gallery-overlay-image').setAttribute('src', pictures[0].url);
-  galleryOverlay.querySelector('.likes-count').textContent = pictures[0].likes;
-  galleryOverlay.querySelector('.comments-count').textContent = pictures[0].comments;
+// ---------> Обработчики событий
+// Открытие галереи
+function galleryOpen() {
   galleryOverlay.classList.remove('hidden');
-  evt.target.stopPropagation();
-}, true);
+  listPictures.removeEventListener('click', onPictureClick);
+  galleryOverlayClose.addEventListener('click', onCloseCrossClick);
+  galleryOverlayClose.addEventListener('keydown', onCloseCrossEnterPress);
+  document.addEventListener('keydown', onGalleryEscPress);
+}
 
+// Закрытие галерии
+function galleryClose() {
+  galleryOverlay.classList.add('hidden');
+  listPictures.addEventListener('click', onPictureClick);
+  galleryOverlayClose.removeEventListener('click', onCloseCrossClick);
+  galleryOverlayClose.removeEventListener('keydown', onCloseCrossEnterPress);
+  document.removeEventListener('keydown', onGalleryEscPress);
+}
+
+// Клик на фотографии
+function onPictureClick(event) {
+  event.preventDefault();
+  var clickTarget = event.target;
+
+  while (clickTarget !== listPictures) {
+    if (clickTarget.classList.contains('picture')) {
+      setPhotoToOverlay(clickTarget);
+      galleryOpen();
+      break;
+    }
+    clickTarget = clickTarget.parentElement;
+  }
+}
+
+// Клик на крестике галереи
+function onCloseCrossClick(event) {
+  galleryClose();
+}
+
+// Нажатие Enter на крестике галереи
+function onCloseCrossEnterPress(event) {
+  if (event.keyCode === ENTER_KEYCODE) {
+    galleryClose();
+  }
+}
+// Нажатие на ESC при открытой галерее
+function onGalleryEscPress(event) {
+  if (event.keyCode === ESC_KEYCODE) {
+    galleryClose();
+  }
+}
+// <------ Обработчики событий
+
+listPictures.addEventListener('click', onPictureClick);
+document.querySelector('.upload-overlay').classList.add('hidden');
