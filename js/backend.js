8939aaabe76a0e0edb3765f069bb86b53fb2d741
Ekaterina <popova_katyusha90@mail.ru>
2017-09-06 13:58:24 +0300

    Выполнено задание 20

---
+++ js/backend.js
@@ -0,0 +1,40 @@
+'use strict';
+
+(function () {
+  var SERVER_URL = 'https://1510.dump.academy/kekstagram';
+
+  var setup = function (onSuccess, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onSuccess(xhr.response);
+      } else {
+        onError(xhr.response);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'mc');
+    });
+    xhr.timeout = 10000;
+    return xhr;
+  };
+
+  window.backend = {
+    save: function (data, onSuccess, onError) {
+      var xhr = setup(onSuccess, onError);
+
+      xhr.open('POST', SERVER_URL);
+      xhr.send(data);
+    },
+    load: function (onSuccess, onError) {
+      var xhr = setup(onSuccess, onError);
+
+      xhr.open('GET', SERVER_URL + '/data');
+      xhr.send();
+    }
+  };
+})();
